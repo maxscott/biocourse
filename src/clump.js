bb46408d885d0402fd42dc6d3eaa4ac220b958f9
Max <max.weinbrown@gmail.com>
2013-11-17 20:50:12 -0500

    finished clumping, not good enough for e coli :(

--- src/clump.js
+++ src/clump.js
@@ -1,17 +1,55 @@
 fs = require('fs');
 
-function runClumps(k, L, t, data) {
-  // todo
+function frequencies(data,k){
+
+  freq = {};
+
+  for(var i = 0; i < data.length-k; i++){
+    
+    curr = data.substring(i, i+k);
+    
+    if(freq[curr] > 0){
+      freq[curr] = freq[curr] + 1;
+    }
+    else{
+      freq[curr] = 1;
+    }      
+  }
+
+  return freq;
 }
 
-function processFile(err, data) {
+function runClumps(k, L, t, data) {
 
-  lines = data.toString().split("\n");
-  data = lines[0];
-  pattern = lines[1].split(" ");
-  k = pattern[0],
-  L = pattern[1],
-  t = pattern[2];
+  clumps = [];
+
+  for(i = 0; i < (data.length-L); i++) {
+
+    windowedData = data.substring(i, i+L);
+    freq = frequencies(windowedData, k);
+
+    for(var x in freq) {
+
+      // correct t number, and unique
+      if(freq[x] == t && clumps.indexOf(x) == -1) {
+        clumps.push(x);
+      }
+    }
+  }
+  
+  return clumps;  
+}
+
+function processFile(err, file) {
+
+  lines = file.toString().split("\n");
+  params = lines[1].split(" ");
+
+  data = lines[0].toString();
+
+  k = parseInt(params[0]),
+  L = parseInt(params[1]),
+  t = parseInt(params[2]);
 
   matches = runClumps(k, L, t, data);
   console.log(matches.join(" "));  
